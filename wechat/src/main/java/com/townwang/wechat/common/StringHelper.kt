05b777e745333853e7f83b3b4d3cdbab0782f7f4
Townwang <android@townwang.com>
2021-02-24 00:00:18 +0800

    publish v1.0.3
    Signed-off-by: Townwang <android@townwang.com>

--- wechat/src/main/java/com/townwang/wechat/common/StringHelper.kt
+++ wechat/src/main/java/com/townwang/wechat/common/StringHelper.kt
@@ -1,13 +1,21 @@
 package com.townwang.wechat.common
-
 import com.townwang.wechat.BuildConfig
-
+import com.townwang.wechat.SECRET_LETTER
 fun String.replaceThis(): String {
     return  replace(BuildConfig.TOWN_THIS,".")
 }
 fun String.removeSeizeSeat(): String {
     return replace(BuildConfig.TOWN_SEIZE,"")
 }
+
+fun String.decrypt():String{
+    val a: CharArray = toCharArray()
+    for (i in a.indices) {
+        a[i] = (a[i] - SECRET_LETTER)
+    }
+    return String(a)
+}
+
 fun String.restoreAllIllusion(): String {
     return replaceThis().removeSeizeSeat()
 }
